b9c748267b42b10a2d285bb63691e999159a498a
Dave <DavidRockin@users.noreply.github.com>
2024-04-10 22:55:40 -0400

    change client finding primary

--- client.py
+++ client.py
@@ -5,17 +5,47 @@
 import grpc
 import replication_pb2
 import replication_pb2_grpc
+from server_registry import ports
+
+
+primaryPort = ''
 
 # waits for acknowledgement from priamry server then writes to log file
 def write_to_primary(key, value):
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = replication_pb2_grpc.SequenceStub(channel)
-        response = stub.Write(replication_pb2.WriteRequest(key=key, value=value))
-        print("Response from Primary:", response.ack)
+    global primaryPort
+    uris = ports.copy()
+    
+    if primaryPort is not None and len(primaryPort) > 0:
+        uris.insert(0, primaryPort)
 
-        # Log the write operation
-        with open('client.txt', 'a') as f:
-            f.write(f"Key: {key}, Value: {value}\n")
+    while len(uris) > 0:
+        port = uris.pop(0)
+
+        try:
+            with grpc.insecure_channel('localhost:%s'%port) as channel:
+                stub = replication_pb2_grpc.SequenceStub(channel)
+                response = stub.Write(replication_pb2.WriteRequest(key=key, value=value))
+
+                # were we instructed to use a different leader ??
+                if response.leaderId > 0:
+                    uris.insert(0, str(response.leaderId))
+                    continue
+
+                print("Response from Primary:", response.ack)
+
+                # Log the write operation
+                with open('client.txt', 'a') as f:
+                    f.write(f"Key: {key}, Value: {value}\n")
+                
+                # update the primary's port
+                primaryPort = port
+                print('[!] NEW PRIMARY PORT = %s' % primaryPort)
+
+                return
+        except grpc.RpcError as ex:
+            print('[ERROR] ', ex)
+    
+    print('[ERROR] Failed to write to any primary')
 
 if __name__ == "__main__":
     with open('client.txt', 'w'): pass  # Clear client log file
